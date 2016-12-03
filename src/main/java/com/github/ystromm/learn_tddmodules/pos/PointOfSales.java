62460f0207fe224e275e3cfbd7de1d912d3a1503
Martin Carlsson <martin.carlsson@omegapoint.se>
2016-12-03 11:14:50 +0100

    Added test for empty barcode

--- src/main/java/com/github/ystromm/learn_tddmodules/pos/PointOfSales.java
+++ src/main/java/com/github/ystromm/learn_tddmodules/pos/PointOfSales.java
@@ -1,11 +1,15 @@
 package com.github.ystromm.learn_tddmodules.pos;
 
+import java.util.Optional;
+
 public class PointOfSales {
-    public Price getPrice(Barcode barcode) {
+    public Optional<Price> getPrice(Barcode barcode) {
         if (barcode.getValue().equals("1234567890")) {
-            return Price.of(37);
+            return Optional.of(Price.of(37));
+        } else if (barcode.getValue().equals("2222222222")) {
+            return Optional.of(Price.of(73));
         } else {
-            return Price.of(73);
+            return Optional.empty();
         }
     }
 }
--- src/test/java/com/github/ystromm/learn_tddmodules/pos/PointOfSalesTest.java
+++ src/test/java/com/github/ystromm/learn_tddmodules/pos/PointOfSalesTest.java
@@ -3,6 +3,8 @@ package com.github.ystromm.learn_tddmodules.pos;
 
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -10,20 +12,27 @@ public class PointOfSalesTest {
 
     public static final Barcode UMBRELLA_BARCODE = Barcode.of("1234567890");
     public static final Barcode RAIN_HAT_BARCODE = Barcode.of("2222222222");
+    public static final Barcode EMPTY_BARCODE = Barcode.of("");
     public static final Price RAIN_HAT_PRICE = Price.of(73);
     public static final Price UMBRELLA_PRICE = Price.of(37);
 
     @Test
     public void entering_umbrella_barcode_should_return_umbrella_price() {
-        assertThat(getPrice(UMBRELLA_BARCODE), equalTo(UMBRELLA_PRICE));
+        assertThat(getPrice(UMBRELLA_BARCODE), equalTo(Optional.of(UMBRELLA_PRICE)));
     }
 
     @Test
     public void entering_rain_hat_barcode_should_return_rain_hat_price() {
-        assertThat(getPrice(RAIN_HAT_BARCODE), equalTo(RAIN_HAT_PRICE));
+        assertThat(getPrice(RAIN_HAT_BARCODE), equalTo(Optional.of(RAIN_HAT_PRICE)));
     }
 
-    private static Price getPrice(Barcode barcode) {
+    @Test
+    public void entering_empty_barcode_should_absent_price() {
+        assertThat(getPrice(EMPTY_BARCODE), equalTo(Optional.empty()));
+    }
+
+
+    private static Optional<Price> getPrice(Barcode barcode) {
         return new PointOfSales().getPrice(barcode);
     }
 }
