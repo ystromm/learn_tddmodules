486b8226dd4011de319e96b05e5457367ff6e2d4
Martin Carlsson <martin.carlsson@omegapoint.se>
2016-12-03 11:08:29 +0100

    Refactor test.

--- src/test/java/com/github/ystromm/learn_tddmodules/pos/PointOfSalesTest.java
+++ src/test/java/com/github/ystromm/learn_tddmodules/pos/PointOfSalesTest.java
@@ -1,7 +1,6 @@
 package com.github.ystromm.learn_tddmodules.pos;
 
 
-import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,16 +8,22 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class PointOfSalesTest {
 
+    public static final Barcode UMBRELLA_BARCODE = Barcode.of("1234567890");
+    public static final Barcode RAIN_HAT_BARCODE = Barcode.of("2222222222");
+    public static final Price RAIN_HAT_PRICE = Price.of(73);
+    public static final Price UMBRELLA_PRICE = Price.of(37);
+
     @Test
-    public void entering_barcode1_should_return_price() {
-        final Price price = new PointOfSales().getPrice(Barcode.of("1234567890"));
-        assertThat(price, equalTo(Price.of(37)));
+    public void entering_umbrella_barcode_should_return_umbrella_price() {
+        assertThat(getPrice(UMBRELLA_BARCODE), equalTo(UMBRELLA_PRICE));
     }
 
     @Test
-    public void entering_barcode2_should_return_price() {
-        final Price price = new PointOfSales().getPrice(Barcode.of("2222222222"));
-        assertThat(price, equalTo(Price.of(73)));
+    public void entering_rain_hat_barcode_should_return_rain_hat_price() {
+        assertThat(getPrice(RAIN_HAT_BARCODE), equalTo(RAIN_HAT_PRICE));
     }
 
+    private static Price getPrice(Barcode barcode) {
+        return new PointOfSales().getPrice(barcode);
+    }
 }
